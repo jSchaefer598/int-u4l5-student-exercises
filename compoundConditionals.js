fcb848c1e8b4f12c702df24e5727d0238470aaf0
jSchaefer598 <jaidenschaefer2008@gmail.com>
2025-06-16 10:53:00 -0500

    practice commit

--- compoundConditionals.js
+++ compoundConditionals.js
@@ -2,8 +2,8 @@
 
 // 1. Create a secret username and password.
 //  - The variables are declared already. Update the values.
-let secretUsername = ("");
-let secretPassword = ("");
+let secretUsername = ("Jaiden598");
+let secretPassword = ("mu$hu08");
 let button = document.querySelector("button");
 
 button.addEventListener("click", function() {
@@ -13,7 +13,17 @@ button.addEventListener("click", function() {
 
     // 2. Create a compound conditional statement to check for the following:
     //  - If the username AND password is correct, update the text of the message div to say "Success!"
-    
+    if(username === secretUsername && password === secretPassword){
+        message.innerHTML = "Success!";
+    } else if(username === secretUsername && password !== secretPassword){
+        message.innerHTML = "Wrong password - be careful, don't mess this up too many times.";
+    } else if(username !== secretUsername && password === secretPassword){
+        message.innerHTML = "Wrong username - is there a typo?";
+    } else if(username === "" || password === ""){
+        message.innerHTML = "You need to enter SOMETHING.";
+    } else {
+        message.innerHTML = "Incorrect username and password. Hmm. Suspicious.";
+    }
     // CODE SOLO
     // 3. Create additional compound conditionals that check for the following:
     //  - If the username is correct AND the password is incorrect, update the text of the message div to say "Wrong password - be careful, don't mess this up too many times."
